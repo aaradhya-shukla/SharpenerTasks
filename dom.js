e2c3dd343944d1ca4b4dc2dd954a7d5defb08dac
aaradhya-shukla <aaradhya.shukla229@gmail.com>
2023-07-05 18:31:26 +0530

    add feature to change font to bold

--- dom.js
+++ dom.js
@@ -1,1 +1,19 @@
 console.log("hello")
+console.log(document.URL)
+let headtitle= document.getElementById("header-title");
+
+console.log(headtitle);
+headtitle.innerHTML="<h2 style=color:black><b>HELLO</h2>";
+let header = document.getElementById("main-header");
+header.style.borderBottom="solid 8px black";
+
+//changing list element font to bold and making 3rd lkst element green
+//using dom-get by classname
+
+let items=document.getElementsByClassName("list-group-item");
+console.log(items);
+items[2].style.backgroundColor='green';
+for(let i=0; i<items.length;i++){
+    console.log("yes in for")
+    items[i].style.fontWeight="bold";
+}
