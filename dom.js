ae8c08622dc0de2445a11309a530ead346304e28
aaradhya-shukla <aaradhya.shukla229@gmail.com>
2023-07-06 15:49:22 +0530

    Traversing through DOM

--- dom.js
+++ dom.js
@@ -57,13 +57,110 @@
 //     even[j].style.backgroundColor="pink";
 // }
 
-let item=document.querySelector('li:nth-child(2)');
-item.style.backgroundColor="green";
-let item3=document.querySelector('li:nth-child(3)');
-item3.style.color="white";
-let item2=document.querySelectorAll("li");
-item2[1].style.color="green";
-let odd =document.querySelectorAll('.list-group-item:nth-child(odd)');
-for(let i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor="green";
-}
+// let item=document.querySelector('li:nth-child(2)');
+// item.style.backgroundColor="green";
+// let item3=document.querySelector('li:nth-child(3)');
+// item3.style.color="white";
+// let item2=document.querySelectorAll("li");
+// item2[1].style.color="green";
+// let odd =document.querySelectorAll('.list-group-item:nth-child(odd)');
+// for(let i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor="green";
+// }
+
+// let itemList=document.querySelector('#items');
+
+// // itemList.parentNode.style.backgroundColor="pink";
+// // console.log(itemList.parentNode.parentNode);
+// // console.log(itemList.parentElement);
+// // console.log(itemList.children);// use cnildren instead of childNodes
+// // itemList.children[1].style.color="red";
+// // itemList.children[1].style.fontWeight="bold";
+
+// //using last and first element child dont use first child or last child
+// console.log(itemList.lastElementChild);
+// itemList.firstElementChild.innerHTML="<b>hello";
+
+// // creating new elements in DOM
+
+// let div=document.createElement('div');
+// div.id='hello';
+// div.className="hello1";
+// div.setAttribute('title','heloo from div')
+// console.log(div);
+// let text=document.createTextNode("Hello world");
+// //appending this to our new div element
+// div.append(text);
+// console.log(div);
+// //now placing our div at a position in our html file
+// let container=document.querySelector('#main-header .container');
+// console.log(container);
+
+// let h1=document.querySelector('header h1');
+// console.log(h1);
+// div.style.fontSize="30px";
+// container.insertBefore(div,h1);
+
+
+//task-traverse dom elements->last/first child/elementchild ->child Nodes->children
+//->firstsibling->nextsibiling->createelement->createtextnode->insertbefore
+
+//create new list item
+
+// let item=document.createElement('li');
+// item.className="list-group-item";
+// item.setAttribute('title','new item');
+// console.log(item);
+// let text=document.createTextNode('Item 5');
+// item.append(text);
+// console.log(item);
+
+// let items=document.querySelector('#items');
+// item.style.backgroundColor="pink";
+
+// items.insertBefore(item,items.lastElementChild);
+
+// traverse
+
+// let item=document.querySelector("#items");
+// // parentnode
+// console.log(item.parentNode.parentNode.parentNode);
+// // childNodes
+// console.log(item.childNodes);
+// // children
+// console.log(item.children);
+// console.log(item.parentNode.parentNode.children);
+
+// // LAST AND FIRST CHILD
+
+// console.log(item.lastChild);
+// console.log(item.firstChild);
+
+// //last and first element child
+
+// console.log(item.lastElementChild);
+// console.log(item.previousElementSibling);
+// console.log(item.nextElementSibling);
+
+// ADDING HELLO WORLD BEFORE ITEM LISTER
+
+let div=document.createElement('div');
+div.className="hello";
+div.id="newelement";
+div.setAttribute('title','hello there');
+let text=document.createTextNode("Hello world")
+div.append(text);
+div.style.fontWeight="bold";
+
+let container=document.querySelector('header .container');
+let h1=document.querySelector('#header-title');
+container.insertBefore(div,h1);
+
+let item=document.createElement('li');
+item.className="list-group-item";
+item.innerHTML="<b>Hello World";
+console.log(item);
+item.style.color="red";
+
+let list=document.querySelector('#items');
+list.insertBefore(item,list.firstElementChild);
