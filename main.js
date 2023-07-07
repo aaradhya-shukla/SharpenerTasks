878ccf135e99c3cbd13e0d8197563211414608f9
aaradhya-shukla <aaradhya.shukla229@gmail.com>
2023-07-07 21:06:22 +0530

    local storage part2

--- main.js
+++ main.js
@@ -1,26 +1,23 @@
 let form=document.getElementById("my-form");
-form.addEventListener("submit",saveValue);
-let UserId=1;
-function saveValue(e){
-    e.preventDefault();    
+form.addEventListener("submit",saveData);
+function saveData(e){
+    e.preventDefault();
     let name=document.getElementById("name").value;
+    let phone=document.getElementById("phone").value;
     let email=document.getElementById("email").value;
-    if (name==="" || email===""){
-        alert("Please enter credentials ");
-        return;
+    if (name=== '' || phone===''|| email===''){
+        alert("Please fill the credentials")
     }
-
-    let myObj=JSON.stringify({
-        "name":name,
-        "email":email,
-    });
-
-    // localStorage.setItem("name",`${name}`);
-    // localStorage.setItem("email",`${email}`);
-    localStorage.setItem("User Credentials-Id:"+`${UserId}`,myObj);
-    alert("data stored successfully");
+    // converting object to readable form
+    let userObj=JSON.stringify({"name":name,"phone":phone,"email":email});
+    localStorage.setItem(`${email}`,userObj);
+    alert("Data has been saved successfully");
     e.target.reset();
-    UserId+=1;
-    
-    
+    let div=document.getElementById("my-main");
+    let p=document.createElement("p");
+    p.innerText=`${email}`;
+    p.appendChild(document.createElement('br'));
+    console.log(p);
+    console.log(div);
+    div.appendChild(p);
 }
