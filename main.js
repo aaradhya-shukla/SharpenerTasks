8fc65f265b167c00a04246720696fad9f1cc1e21
aaradhya-shukla <aaradhya.shukla229@gmail.com>
2023-07-16 18:58:50 +0530

    Expense Tracker

--- main.js
+++ main.js
@@ -1,66 +1,53 @@
 let form=document.getElementById("my-form");
-let div=document.getElementById("my-main");
-form.addEventListener("submit",saveData);
-//div.addEventListener("click",removeBtn);
+form.addEventListener("submit",addExpense);
+let div=document.getElementsByClassName("row");
+let id=1;
 
-function saveData(e){
+function addExpense(e){
     e.preventDefault();
-    let name=document.getElementById("name").value;
-    let phone=document.getElementById("phone").value;
-    let email=document.getElementById("email").value;
-    if (name=== '' || phone===''|| email===''){
-        alert("Please fill the credentials")
+    let user=document.getElementById("user").value;
+    let expense=document.getElementById("Expense").value;
+    let description=document.getElementById("Description").value;
+    let type=document.querySelector("#Expensetype").value;
+    if(user ==="" || expense === "" || description === ""){
+        alert("Fill details to continue");
+        return;
     }
-    // converting object to readable form
-    let userObj=JSON.stringify({"name":name,"phone":phone,"email":email});
-    localStorage.setItem(`${email}`,userObj);
-    alert("Data has been saved successfully");
-    e.target.reset();
+    
+    let userObj=JSON.stringify({user:user,expense:expense,description:description,type:type});
     let p=document.createElement("p");
-    p.textContent="* "+`${email}`;
-    let del=document.createElement("button");
-    del.className="delete";
-    del.setAttribute("value","Delete");
-    del.textContent="Delete";
-    //edit button
-    let edit=document.createElement("button");
-    edit.className="edit";
-    edit.textContent="Edit";
-    p.appendChild(del);
-    p.appendChild(edit);
-    p.appendChild(document.createElement('br'));
+    p.textContent=` * ${expense} - ${description} - ${type}`
+    let del=document.createElement('button')
+    let edit=document.createElement('button')
+    del.className="delete"
+    del.setAttribute("value","delete expense")
+    del.textContent="delete expense"
+    p.appendChild(del)
+    edit.className=`edit${id}`
+    edit.setAttribute("value","edit expense")
+    edit.textContent="edit expense"
+    p.appendChild(edit)
+    div[0].appendChild(p)
+    del.addEventListener("click",deleteExpense);
+    edit.addEventListener("click",editExpesne);
+    localStorage.setItem(edit.className,userObj)
+    id+=1;
+    form.reset();
 
-    div.appendChild(p);
-    // to delete event
-    del.addEventListener('click',removeBtn);
-    // adding event listner to edit
-    edit.addEventListener('click',editData);
 }
-function removeBtn(e){
-    e.preventDefault();
-    if (e.target.className.contains="delete"){
-        
-        let p=e.target.parentNode;
-        let a=p.parentNode;
-        console.log(p.textContent.substring(2,30));
-        localStorage.removeItem(p.textContent.substring(2,30));
-        div.removeChild(p);
-        
-        // console.log(div.children);
-        // div.removeChild(p);
-    }
+
+function deleteExpense(e){
+    console.log(10000)
+    let div1=e.target.parentNode.parentNode;
+    div1.removeChild(e.target.parentNode)
 }
-function editData(e){
-    e.preventDefault();
-    if (e.target.className.contains="edit"){
-        let p=e.target.parentNode;
-        let a=p.parentNode;
-        console.log(p.textContent.substring(2,30));
-        let obj=JSON.parse(localStorage.getItem(p.textContent.substring(2,30)));
-        localStorage.removeItem(p.textContent.substring(2,30));
-        div.removeChild(p);
-        document.getElementById("name").value=obj.name;
-        document.getElementById("phone").value=obj.phone;
-        document.getElementById("email").value=obj.email;
-    }
+
+function editExpesne(e){
+    let userObj=JSON.parse(localStorage.getItem(e.target.className));
+    let div1=e.target.parentNode.parentNode;
+    div1.removeChild(e.target.parentNode)
+    document.getElementById("user").value=userObj.user;
+    document.getElementById("Expense").value=userObj.expense;
+    document.getElementById("Description").value=userObj.description;
+    document.getElementById("Expensetype").reset;
 }
