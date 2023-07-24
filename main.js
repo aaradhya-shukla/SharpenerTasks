4a9bf1cc21e52d48fe10bee0b8965a87fd5cbe77
aaradhya-shukla <aaradhya.shukla229@gmail.com>
2023-07-24 19:55:32 +0530

    promises

--- main.js
+++ main.js
@@ -1,53 +1,42 @@
-let form=document.getElementById("my-form");
-form.addEventListener("submit",addExpense);
-let div=document.getElementsByClassName("row");
-let id=1;
-
-function addExpense(e){
-    e.preventDefault();
-    let user=document.getElementById("user").value;
-    let expense=document.getElementById("Expense").value;
-    let description=document.getElementById("Description").value;
-    let type=document.querySelector("#Expensetype").value;
-    if(user ==="" || expense === "" || description === ""){
-        alert("Fill details to continue");
-        return;
+const blog=[];
+async function getPosts(){
+    const createPost=(newPost)=>{
+        return new Promise((resolve,reject)=>{
+            setTimeout(()=>{
+                blog.push(newPost);
+                resolve();
+            })
+        })
     }
+    const deletePost=()=>{
+        return new Promise((resolve,reject)=>{
+            setTimeout(()=>{
+                if (blog.length>0){
+                    let value=blog.pop();
+                    resolve(value);
+                }
+                else{
+                    reject("ERROR");
+                }
+            })
+        })
+    }
+    await createPost('post 1');
+    await createPost('post 2');
+    blog.forEach((post)=>{
+        console.log(post);
+    })
+    let delPost;
+    try{
+        delPost=await deletePost();
+    }
+    catch(msg){
+        delPost=msg
+        console.log(delPost);
+        return;
+
+    }
+    console.log(`deleted ${delPost}`);
     
-    let userObj=JSON.stringify({user:user,expense:expense,description:description,type:type});
-    let p=document.createElement("p");
-    p.textContent=` * ${expense} - ${description} - ${type}`
-    let del=document.createElement('button')
-    let edit=document.createElement('button')
-    del.className="delete"
-    del.setAttribute("value","delete expense")
-    del.textContent="delete expense"
-    p.appendChild(del)
-    edit.className=`edit${id}`
-    edit.setAttribute("value","edit expense")
-    edit.textContent="edit expense"
-    p.appendChild(edit)
-    div[0].appendChild(p)
-    del.addEventListener("click",deleteExpense);
-    edit.addEventListener("click",editExpesne);
-    localStorage.setItem(edit.className,userObj)
-    id+=1;
-    form.reset();
-
 }
-
-function deleteExpense(e){
-    console.log(10000)
-    let div1=e.target.parentNode.parentNode;
-    div1.removeChild(e.target.parentNode)
-}
-
-function editExpesne(e){
-    let userObj=JSON.parse(localStorage.getItem(e.target.className));
-    let div1=e.target.parentNode.parentNode;
-    div1.removeChild(e.target.parentNode)
-    document.getElementById("user").value=userObj.user;
-    document.getElementById("Expense").value=userObj.expense;
-    document.getElementById("Description").value=userObj.description;
-    document.getElementById("Expensetype").reset;
-}
+getPosts();
