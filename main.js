afe104582a1791b9149856ea0f40da0f4b997e37
aaradhya-shukla <aaradhya.shukla229@gmail.com>
2023-07-26 19:34:34 +0530

    all functionalities

--- main.js
+++ main.js
@@ -1,47 +1,63 @@
-let form=document.getElementById('my-form');
-form.addEventListener("submit",book);
-let arr=[];
-function book(e){
+show()
+function show(){
+    axios.get('https://crudcrud.com/api/95cfd471008f45b48929c2f6859b352b/book').then((res)=>{
+        for (let i=0;i<res.data.length;i++){
+            let del=document.createElement("button")
+            let edit=document.createElement("button")
+            let ele=document.createElement("p")
+            // setting attribute
+            del.setAttribute("id",`${res.data[i]._id}`)
+            edit.setAttribute("id",`${res.data[i]._id}`)
+            ele.setAttribute("id",`${res.data[i]._id}`)
+            del.textContent="delete"
+            edit.textContent="edit"
+            // adding event listeners
+            del.addEventListener("click",delt);
+            edit.addEventListener("click",editt);
+            // adding to parent 
+            ele.textContent=`${res.data[i].name}-${res.data[i].email}`
+            ele.appendChild(del)
+            ele.appendChild(edit)
+            document.body.appendChild(ele)
+        }
+    }).catch((error)=>{console.log(error)
+    return;})
+
+}
+
+let form=document.getElementById("my-form");
+form.addEventListener("submit",add);
+
+function add(e){
     e.preventDefault()
     let name=document.getElementById("name").value;
     let email=document.getElementById("email").value;
-    let userObj={name:name,email:email}
-    axios.post("https://crudcrud.com/api/95cfd471008f45b48929c2f6859b352b/meet",userObj).then((res)=>{
-        console.log(res.status)
-    }).catch((err)=>console.log(err));
+    let userObj={name:name,email:email};
+    console.log(userObj)
+    //post call
+    axios.post('https://crudcrud.com/api/95cfd471008f45b48929c2f6859b352b/book',userObj).then((res)=>show()).
+    catch((err)=>console.log(err))
     form.reset()
-    axios.get("https://crudcrud.com/api/95cfd471008f45b48929c2f6859b352b/meet").then((res)=>{
-        showData(res)
-    }).catch((err)=>{
-        console.log(err)
-    })
-}
-
-function showData(res){
-    for(let i=0; i<res.data.length;i++){
-        let del=document.createElement("button");
-        let edit=document.createElement("button")
-        del.setAttribute('id',`${res.data[i]._id}`)
-        edit.setAttribute('id',`${res.data[i]._id}`);
-        del.textContent='delete'
-        edit.textContent='edit'
-
-        del.addEventListener("click",delt)
-        let ele=document.createElement('p')
-        ele.innerHTML=`<h3>${res.data[i].name}-${res.data[i].email}</h3>`;
-        ele.appendChild(del)
-        ele.appendChild(edit)
-        document.body.appendChild(ele)
-    }
 }
 
 function delt(e){
-    console.log(e.target.id)
-    axios.delete(`https://crudcrud.com/api/95cfd471008f45b48929c2f6859b352b/meet/${e.target.id}`).then((res)=>
-    console.log("res.status")).catch((err)=>console.log(err))
+    document.body.removeChild(e.target.parentNode);
+    axios.delete(`https://crudcrud.com/api/95cfd471008f45b48929c2f6859b352b/book/${e.target.id}`).then((res)=>
+    console.log(res.status)).catch((err)=>console.log(err));
+
 }
-function edit(e){
+
+function editt(e){
+    document.body.removeChild(e.target.parentNode);
+    axios.get(`https://crudcrud.com/api/95cfd471008f45b48929c2f6859b352b/book/${e.target.id}`).then((res)=>{
+        update(res)
+    }).catch((error)=>console.log(error))
+    axios.delete(`https://crudcrud.com/api/95cfd471008f45b48929c2f6859b352b/book/${e.target.id}`).then((res)=>
+    console.log(res.status)).catch((err)=>console.log(err));
+}
+
+function update(res){
+    document.getElementById("name").value=res.data.name
+    document.getElementById("email").value=res.data.email
     
-    axios.put(`https://crudcrud.com/api/95cfd471008f45b48929c2f6859b352b/meet/${e.target.id}`,).then((res)=>
-    console.log("res.status")).catch((err)=>console.log(err))
 }
